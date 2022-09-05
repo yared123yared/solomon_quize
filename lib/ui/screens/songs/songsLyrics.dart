355727a36c5519b83e81810e1676b6461b8eaf51
jituji <rsr.jitu@gmail.com>
2022-09-06 05:00:09 +0530

    Updated @05PM 06/09

---
+++ lib/ui/screens/songs/songsLyrics.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:http/http.dart' as http;
+
+// ignore: camel_case_types
+class songsLyrics extends StatefulWidget {
+  const songsLyrics({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
